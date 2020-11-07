bad36a9de5cccbf5dde160f7f93a4a6087170402
Shurkovalina <shurkowalina@yandex.ru>
2020-11-07 11:09:58 +0300

    added program1

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,11 @@
 public class Main {
     public static void main(String[] args) {
         CreditPaymentService service = new CreditPaymentService();
-        double monthlyPayment = service.calculate(1_000_000, 36, 9.99);
-        System.out.println(monthlyPayment);
+        double monthlyPayment1 = service.calculate(1_000_000, 12, 9.99);
+        System.out.println(monthlyPayment1);
+        double monthlyPayment2 = service.calculate(1_000_000, 24, 9.99);
+        System.out.println(monthlyPayment2);
+        double monthlyPayment3 = service.calculate(1_000_000, 36, 9.99);
+        System.out.println(monthlyPayment3);
     }
 }
